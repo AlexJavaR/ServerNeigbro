31c9e5616be521640bdfb6d2145c8163b909b047
Alex Meleshko <alemeleshko@gmail.com>
2017-03-22 12:18:26 +0200

    added sending email for withdrawal money and feedback form from website

---
+++ src/main/java/com/prototype/config/MailConfig.java
@@ -0,0 +1,34 @@
+package com.prototype.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class MailConfig {
+    @Bean
+    public JavaMailSender javaMailService() {
+        JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
+
+        javaMailSender.setHost("smtp.gmail.com");
+        javaMailSender.setPort(587);
+        javaMailSender.setUsername("neighbro.app");
+        javaMailSender.setPassword("trololo56789");
+
+        javaMailSender.setJavaMailProperties(getMailProperties());
+
+        return javaMailSender;
+    }
+
+    private Properties getMailProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("mail.transport.protocol", "smtp");
+        properties.setProperty("mail.smtp.starttls.enable", "true");
+        //properties.setProperty("mail.smtp.auth", "false");
+        //properties.setProperty("mail.debug", "false");
+        return properties;
+    }
+}
